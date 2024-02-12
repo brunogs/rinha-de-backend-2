e42c65fc8b831708b3c33ee255a3cb8919ae173a
Bruno Silva <bruno.silva@digibee.com>
2024-02-12 11:00:03 -0300

    comprimido respostas

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"api/api"
 	"context"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"time"
 )
 
 func main() {
@@ -15,8 +18,11 @@ func main() {
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
+	logger := zap.NewNop()
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
 	handler := api.NewGinHandler(queries)
 	handler.SetupEndpoints(r)
+
 	if err = r.Run(":3000"); err != nil {
 		panic("Failed to run gin server")
 	}
